5cf801207513c80e887e890ba54eaa0316f722e5
yank1 <yancarloglez98@gmail.com>
2022-08-05 14:22:54 -0400

    add public route

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppConfigService } from './shared/modules/config/service/app-config-service';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {AppConfigService} from './shared/modules/config/service/app-config-service';
+import * as express from 'express'
+import {join} from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
-    // allowedHeaders: [],
-  });
+    app.enableCors({
+        origin: '*',
+        methods: ['POST', 'PUT', 'DELETE', 'GET'],
+        // allowedHeaders: [],
+    });
 
-  const configService = app.get(AppConfigService);
-  await app.listen(configService.app.port);
+    app.use('/public', express.static(join(process.cwd(), 'public')))
+    const configService = app.get(AppConfigService);
+    await app.listen(configService.app.port);
 
-  console.log(`App run in port ${configService.app.port}`);
+    console.log(`App run in port ${configService.app.port}`);
 
 }
 
